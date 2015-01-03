2f7dc5e86a5376d5c5988acece34802ab432558d
tonitakekawa <tointakekawa@gmail.com>
2015-01-03 10:41:44 +0900

    first commit
    ルビーの練習

---
+++ FizzBuzz/FizzBuzz2.rb
@@ -0,0 +1,11 @@
+(1..100).each do |num|
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
+end
---
+++ FizzBuzz/fizzbuzz3.rb
@@ -0,0 +1,13 @@
+class Fixnum
+  def fizzbuzz
+    return :FizzBuzz if self % 15 == 0
+    return :Buzz if self % 5 == 0
+    return :Fizz if self % 3 == 0
+
+    self
+  end
+end
+
+(1..100).map {|num| num.fizzbuzz}.each do |num|
+  puts num
+end
